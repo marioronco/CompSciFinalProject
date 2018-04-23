dc2f9fd18c32eb79c5797b00a9d9c46326f5f43c
marioronco <38333605+marioronco@users.noreply.github.com>
2018-04-22 18:40:22 -0700

    FinalCompSciProject
    editing the comments to reduce lines of code. still need to edit colors and wording to make it easier to read
    
    Co-Authored-By: wmeinhardt1 <wmeinhardt1@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
- #Sample Python/Pygame Programs
-# Simpson College Computer Science
-# http://programarcadegames.com/
-# http://simpson.edu/computer-science/
+#creating a game using the "sprite" function in pygame
  
 import pygame
 import random
 
-# here's a new comment
-# here's another comment
+
  
-# Define some colors
+# Defining color parameters
 BLACK    = (   0,   0,   0)
 WHITE    = ( 255, 255, 255)
 RED      = ( 255,   0,   0)
  
-# This class represents the ball        
-# It derives from the "Sprite" class in Pygame
+# Parameters represent the ball        
+#"Sprite" class in Pygame
 class Block(pygame.sprite.Sprite):
      
     # Constructor. Pass in the color of the block, 
@@ -24,27 +20,21 @@ class Block(pygame.sprite.Sprite):
         # Call the parent class (Sprite) constructor
         super().__init__() 
  
-        # Create an image of the block, and fill it with a color.
-        # This could also be an image loaded from the disk.
+        # using block image and filling it.
         self.image = pygame.Surface([width, height])
         self.image.fill(color)
  
-        # Fetch the rectangle object that has the dimensions of the image
-        # image.
-        # Update the position of this object by setting the values 
-        # of rect.x and rect.y
         self.rect = self.image.get_rect()
  
-# Initialize Pygame
+#Pygame
 pygame.init()
  
 # Set the height and width of the screen
 SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 400
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
- 
-# This is a list of 'sprites.' Each block in the program is
-# added to this list. The list is managed by a class called 'Group.'
+
+#list of "sprites" within pygame that manages the blocks in groups
 block_list = pygame.sprite.Group()
  
 # This is a list of every sprite. All blocks and the player block as well.
@@ -81,24 +71,17 @@ score = 0
 # Current level
 level = 1
  
-# -------- Main Program Loop -----------
 while not done:
-    # ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
      
-    for event in pygame.event.get(): # User did something
+    for event in pygame.event.get(): 
         if event.type == pygame.QUIT: # If user clicked close
             done = True # Flag that we are done so we exit this loop
  
     # Get the current mouse position. This returns the position
     # as a list of two numbers.
     pos = pygame.mouse.get_pos()
-     
-    # ALL EVENT PROCESSING SHOULD GO ABOVE THIS COMMENT
- 
-    # ALL GAME LOGIC SHOULD GO BELOW THIS COMMENT
-     
-    # Fetch the x and y out of the list, 
-    # just like we'd fetch letters out of a string.
+   
+    
     # Set the player object to the mouse location
     player.rect.x = pos[0]
     player.rect.y = pos[1]
@@ -106,20 +89,17 @@ while not done:
     # See if the player block has collided with anything.
     blocks_hit_list = pygame.sprite.spritecollide(player, block_list, True)  
      
-    # Check the list of collisions.
+    #collisons
     for block in blocks_hit_list:
         score += 1
         print( score )
  
-    # Check to see if all the blocks are gone.
-    # If they are, level up.
+    # Check to see if all the blocks are gone. If they are, level up.
     if len(block_list) == 0:
         # Add one to the level
         level += 1
  
-        # Add more blocks. How many depends on the level.
-        # Also, an 'if' statement could be used to change what
-        # happens customized to levels 2, 3, 4, etc.
+        # Add more blocks. Adds more depending on the level.
         for i in range(level * 10):
             # This represents a block
             block = Block(BLACK, 20, 15)
@@ -131,10 +111,7 @@ while not done:
             # Add the block to the list of objects
             block_list.add(block)
             all_sprites_list.add(block)
- 
-    # ALL GAME LOGIC SHOULD GO ABOVE THIS COMMENT
- 
-    # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT    
+  
  
     # Clear the screen
     screen.fill(WHITE)
@@ -147,14 +124,11 @@ while not done:
          
     text = font.render("Level: "+str(level), True, BLACK)
     screen.blit(text, [10, 40])
-     
-    # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
-     
-    # Go ahead and update the screen with what we've drawn.
+    
     pygame.display.flip()
  
-    # Limit to 60 frames per second
-    clock.tick(60)
+    # Limit to 80 frames per second
+    clock.tick(80)
  
  
 pygame.quit()
